4b1ea6949197e98faf3ef7e1943d2ef3415ab6f5
codeviking <skoneberg@gmail.com>
2017-05-10 22:26:30 -0700

    Add the first version of the plugin.
    * Add a basic manifest.
    * Add a script that executes on a github PR and replaces each line
      of markdown with a rendered HTML version.

---
+++ README.md
@@ -0,0 +1,3 @@
+# gh-review-markdown
+
+A Google Chrome extension that makes it easy(ish) to review markdown documents present in Github pull requests.
---
+++ src/render-markdown.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** 
+ * TODO:
+ *  - Make it pretty (Github uses a monospace font, I need to figure out how to make it "code")
+ *  - Figure out how to support tables (it might be hard, given the parser can't handle it 
+ *    line by line (which makes sense). This might mean I have to re-render the whole markdown 
+ *    document, rather than do it line by line (which could get whacky w/ respect to using Github's
+ *    built in comments capability)
+ */
+
+function isMarkdown(fileNode) {
+  const fileNameNode = fileNode.querySelectorAll('.file-info a')[0];
+  return fileNameNode && fileNameNode.textContent.endsWith('.md');
+}
+
+function replaceMarkdownWithHtml(fileNode) {
+  // Find each line of code, which in this case is markdown
+  const markdownNodes = fileNode.querySelectorAll('.blob-code .blob-code-inner');
+  const markdownContent = Array.from(markdownNodes).map(node => {
+    let content = node.textContent;
+    if (content[0] === '+' || content[0] === '-') {
+      content = content.substr(1);
+    }
+    // Replace the HTML with the markdown version.
+    node.innerHTML = markdown.toHTML(content)
+  });
+}
+
+console.log('Markdown renderer intialized...');
+
+Array.from(document.querySelectorAll('#files .file') || [])
+  .filter(isMarkdown)
+  .forEach(replaceMarkdownWithHtml);
+
+console.log('Complete, all markdown has been replaced with HTML.');
